1a49e9f5c595871c455af5d4e831a6e6e5b0229b
Jishnu <e0725046@u.nus.edu>
2023-10-31 19:09:16 +0800

    WIP: update datePicker component

--- pages/itinerary/datePicker.tsx
+++ pages/itinerary/datePicker.tsx
@@ -10,7 +10,9 @@ import {
   StyleSheet,
   StatusBar,
   Platform,
+  View,
 } from "react-native";
+import Calendar from "react-native-calendar-date-range-picker";
 
 export default function DatePicker() {
   const [date, setDate] = useState(new Date());
@@ -40,14 +42,26 @@ export default function DatePicker() {
   return (
     <NativeBaseProvider>
       <SafeAreaView style={styles.container}>
-        <Box w="90%" maxW="400" paddingTop={4} paddingBottom={8}>
-          <Progress value={25} mx="4" />
+        <Box
+          w="90%"
+          maxW="400"
+          paddingTop={4}
+          paddingBottom={8}
+          alignSelf={"center"}
+        >
+          <Progress
+            value={25}
+            mx="4"
+            backgroundColor={"#F8FAF0"}
+            _filledTrack={{ backgroundColor: "#9CADA4" }}
+          />
         </Box>
         <Text
           style={{
             fontFamily: "Bitter-Bold",
             fontSize: 32,
             fontWeight: "bold",
+            paddingLeft: 20,
             paddingBottom: 20,
             textAlign: "left",
           }}
@@ -56,6 +70,34 @@ export default function DatePicker() {
         </Text>
         <Text>selected: {date.toLocaleString()}</Text>
         <Button onPress={showDatepicker} title="Select a date" />
+        <Box width={"95%"} alignSelf={"center"}>
+          <Calendar
+            subtitle={(count) => `${count} nights`}
+            monthHeight={300}
+            renderWeekTextComponent={(weekdayTitle) => (
+              <Text style={styles.monthTextStyle} key={weekdayTitle}>
+                {weekdayTitle}
+              </Text>
+            )}
+            renderMonthTextComponent={(monthName) => (
+              <Text>
+                {`${monthName.format("MMMM")} ${monthName
+                  .clone()
+                  .locale("en")
+                  .format("YYYY")}`}
+              </Text>
+            )}
+            renderFooterComponent={(handleDonePressed, daysDifference) => (
+              <View style={{ alignItems: "center" }}>
+                <View style={styles.dividerStyle} />
+                <Button
+                  onPress={handleDonePressed}
+                  title={`Done (${daysDifference} selected)`}
+                />
+              </View>
+            )}
+          />
+        </Box>
         {show && (
           <DateTimePicker
             testID="dateTimePicker"
@@ -74,5 +116,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#E5E8D9",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  monthTextStyle: {
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  buttonStyle: {
+    marginTop: 5,
+  },
+  dividerStyle: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "grey",
   },
 });
